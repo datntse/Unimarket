799620bb7e75073c833dfae3cc88ebb041630a95
cuongphan <cuongpvpvc@gmail.com>
2024-06-18 15:02:50 +0700

    publish

--- Unimarket/Program.cs
+++ Unimarket/Program.cs
@@ -86,12 +86,10 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
